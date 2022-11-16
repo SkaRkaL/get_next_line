e1987b6d35598a5a30997153f695b276e0a2fd09
Saad Karkal <sakarkal@e1r3p7.1337.ma>
2022-11-16 05:30:44 +0100

    G_N_L

--- get_next_line.c
+++ get_next_line.c
@@ -6,17 +6,17 @@
 /*   By: sakarkal <sakarkal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/13 10:29:00 by sakarkal          #+#    #+#             */
-/*   Updated: 2022/11/13 17:08:31 by sakarkal         ###   ########.fr       */
+/*   Updated: 2022/11/16 05:27:50 by sakarkal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include"get_next_line.h"
+#include "get_next_line.h"
 
-char	*ft_get_line(char *save)
+char	*the_line(char *save)
 {
 	int		i;
 	char	*s;
- 
+
 	i = 0;
 	if (!save[i])
 		return (NULL);
@@ -39,13 +39,13 @@ char	*ft_get_line(char *save)
 	s[i] = '\0';
 	return (s);
 }
- 
-char	*ft_save(char *save)
+
+char	*saver(char *save)
 {
 	int		i;
 	int		c;
 	char	*s;
- 
+
 	i = 0;
 	while (save[i] && save[i] != '\n')
 		i++;
@@ -65,12 +65,12 @@ char	*ft_save(char *save)
 	free(save);
 	return (s);
 }
- 
-char	*ft_read_and_save(int fd, char *save)
+
+char	*ft_read_save(int fd, char *save)
 {
 	char	*buff;
 	int		read_bytes;
- 
+
 	read_bytes = 1;
 	while (!ft_search(save, '\n') && read_bytes != 0)
 	{
@@ -87,7 +87,6 @@ char	*ft_read_and_save(int fd, char *save)
 		save = ft_strjoin(save, buff);
 		free(buff);
 	}
-
 	return (save);
 }
 
@@ -95,12 +94,12 @@ char	*get_next_line(int fd)
 {
 	char		*line;
 	static char	*save;
-	char 		*temp;
- 
+	char		*temp;
+
 	line = 0;
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (0);
-	temp = ft_read_and_save(fd, save);
+	temp = ft_read_save(fd, save);
 	if (!temp)
 	{
 		if (save)
@@ -110,17 +109,7 @@ char	*get_next_line(int fd)
 	}
 	save = temp;
 	if (save)
-		line = ft_get_line(save);
-	save = ft_save(save);
+		line = the_line(save);
+	save = saver(save);
 	return (line);
-}
-
-int main()
-{
-	int fd = open("text.txt", O_RDWR, 777);
-
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
 }
